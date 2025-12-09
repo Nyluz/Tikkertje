430940346827ea7f6fd3f6109f1f596bee042c74
Niels Scherpenkate <niels360@live.nl>
2025-12-09 22:06:39 +0100

    Added player controller, made ragdoll standup after falling

--- Assets/Scripts/HitTarget.cs
+++ Assets/Scripts/HitTarget.cs
@@ -3,8 +3,28 @@ using UnityEngine.InputSystem;
 
 public class HitTarget : MonoBehaviour
 {
+    [SerializeField]
+    private CharacterController characterController;
+
     private Camera m_camera;
+
     public float maximumForce;
+
+    private float velocity;
+
+    [SerializeField]
+    private float velocityMultiplier = 4f;
+
+    public Texture2D crosshairTexture;
+
+    void OnGUI()
+    {
+        float size = 32;
+        float x = (Screen.width - size) / 2f;
+        float y = (Screen.height - size) / 2f;
+
+        GUI.DrawTexture(new Rect(x, y, size, size), crosshairTexture);
+    }
 
     void Awake()
     {
@@ -13,9 +33,12 @@ public class HitTarget : MonoBehaviour
 
     void Update()
     {
-        if (Mouse.current.leftButton.isPressed == true)
+        velocity = characterController.velocity.magnitude * 5f;
+
+        if (Mouse.current.leftButton.isPressed)
         {
-            Ray ray = m_camera.ScreenPointToRay(Pointer.current.position.ReadValue());
+            Ray ray = new Ray(m_camera.transform.position, m_camera.transform.forward);
+
             if (Physics.Raycast(ray, out RaycastHit hitInfo))
             {
                 Ragdoll ragdoll = hitInfo.collider.GetComponentInParent<Ragdoll>();
@@ -24,10 +47,10 @@ public class HitTarget : MonoBehaviour
                     Vector3 forceDirection = ragdoll.transform.position - m_camera.transform.position;
                     forceDirection.y = 1;
                     forceDirection.Normalize();
-                    Vector3 force = forceDirection * maximumForce;
+
+                    Vector3 force = forceDirection * (maximumForce + velocity);
 
                     ragdoll.TriggerRagdoll(force, hitInfo.point);
-
                 }
             }
         }
--- Assets/Scripts/Ragdoll.cs
+++ Assets/Scripts/Ragdoll.cs
@@ -2,20 +2,78 @@ using System.Linq;
 using UnityEngine;
 public class Ragdoll : MonoBehaviour
 {
+    private class BoneTransform
+    {
+        public Vector3 Position { get; set; }
+        public Quaternion Rotation { get; set; }
+    }
+
     private enum CharacterState
     {
         Idle,
-        Ragdoll
+        Ragdoll,
+        StandingUp,
+        ResettingBones
     }
+
+    [SerializeField]
+    private string faceUpstandUpStateName;
+
+    [SerializeField]
+    private string faceDownstandUpStateName;
+
+    [SerializeField]
+    private string faceUpStandUpClipName;
+
+    [SerializeField]
+    private string faceDownStandUpClipName;
+
+    [SerializeField]
+    private float timeToResetBones;
 
     private Rigidbody[] ragdollRigidbodies;
     private Animator animator;
     private CharacterState characterState = CharacterState.Idle;
+    [SerializeField]
+    private float timeToWakeUp;
+    private float timer;
+
+    private Transform hipsBone;
+
+    private BoneTransform[] faceUpStandUpBoneTransforms;
+    private BoneTransform[] faceDownStandUpBoneTransforms;
+
+    private BoneTransform[] ragdollBoneTransforms;
+    private Transform[] bones;
+    private float elapsedResetBonesTime;
+
+    private bool isFacingUp;
 
     void Awake()
     {
         ragdollRigidbodies = GetComponentsInChildren<Rigidbody>();
         animator = GetComponent<Animator>();
+
+        hipsBone = animator.GetBoneTransform(HumanBodyBones.Hips);
+        bones = hipsBone.GetComponentsInChildren<Transform>();
+
+        faceUpStandUpBoneTransforms = new BoneTransform[bones.Length];
+        faceDownStandUpBoneTransforms = new BoneTransform[bones.Length];
+
+        ragdollBoneTransforms = new BoneTransform[bones.Length];
+
+        for (int i = 0; i < bones.Length; i++)
+        {
+            faceUpStandUpBoneTransforms[i] = new BoneTransform();
+            faceDownStandUpBoneTransforms[i] = new BoneTransform();
+
+            ragdollBoneTransforms[i] = new BoneTransform();
+        }
+
+
+        PopulateAnimationStartBoneTransforms(faceUpStandUpClipName, faceUpStandUpBoneTransforms);
+        PopulateAnimationStartBoneTransforms(faceDownStandUpClipName, faceDownStandUpBoneTransforms);
+
         DisableRagdoll();
     }
 
@@ -30,7 +88,12 @@ public class Ragdoll : MonoBehaviour
             case CharacterState.Ragdoll:
                 RagdollBehaviour();
                 break;
-
+            case CharacterState.StandingUp:
+                StandingUpBehaviour();
+                break;
+            case CharacterState.ResettingBones:
+                ResetBonesBehaviour();
+                break;
         }
     }
 
@@ -68,8 +131,131 @@ public class Ragdoll : MonoBehaviour
 
     }
 
+    private void StandingUpBehaviour()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName(GetStandUpStateName()) == false)
+        {
+            characterState = CharacterState.Idle;
+        }
+    }
+
+    private void ResetBonesBehaviour()
+    {
+        elapsedResetBonesTime += Time.deltaTime;
+        float elapsedPercentage = elapsedResetBonesTime / timeToResetBones;
+
+        BoneTransform[] standUpBoneTransforms = GetStandUpBonesTransforms();
+
+        for (int i = 0; i < bones.Length; i++)
+        {
+            bones[i].localPosition = Vector3.Lerp(
+                ragdollBoneTransforms[i].Position,
+                standUpBoneTransforms[i].Position,
+                elapsedPercentage);
+
+            bones[i].localRotation = Quaternion.Lerp(
+                ragdollBoneTransforms[i].Rotation,
+                standUpBoneTransforms[i].Rotation, elapsedPercentage);
+        }
+
+        if (elapsedPercentage >= 1)
+        {
+            characterState = CharacterState.StandingUp;
+            DisableRagdoll();
+            animator.Play(GetStandUpStateName(), 0, 0f);
+        }
+    }
+
     private void RagdollBehaviour()
     {
+        timer += Time.deltaTime;
+        if (timer >= timeToWakeUp)
+        {
+            timer = 0;
+            isFacingUp = hipsBone.forward.y > 0f;
+            AlignPositionToHips();
+            AlignRotationToHips();
 
+            PopulateBoneTransforms(ragdollBoneTransforms);
+
+            characterState = CharacterState.ResettingBones;
+            elapsedResetBonesTime = 0;
+        }
+    }
+
+    private void AlignRotationToHips()
+    {
+        Vector3 originalHipsPosition = hipsBone.position;
+        Quaternion originalHipsRotation = hipsBone.rotation;
+        Vector3 desiredDirection = hipsBone.up;
+        if (isFacingUp)
+        {
+            desiredDirection *= -1;
+        }
+
+        desiredDirection.y = 0;
+        desiredDirection.Normalize();
+
+        Quaternion fromToRotation = Quaternion.FromToRotation(transform.forward, desiredDirection);
+        transform.rotation *= fromToRotation;
+
+        hipsBone.position = originalHipsPosition;
+        hipsBone.rotation = originalHipsRotation;
+    }
+
+    private void AlignPositionToHips()
+    {
+        Vector3 originalHipsPosition = hipsBone.position;
+        transform.position = hipsBone.position;
+
+        Vector3 positionOffset = GetStandUpBonesTransforms()[0].Position;
+        positionOffset.y = 0;
+        positionOffset = transform.rotation * positionOffset;
+        transform.position -= positionOffset;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hitInfo))
+        {
+            transform.position = new Vector3(transform.position.x, hitInfo.point.y, transform.position.z);
+        }
+
+        hipsBone.position = originalHipsPosition;
+    }
+
+    private void PopulateBoneTransforms(BoneTransform[] boneTransforms)
+    {
+        for (int i = 0; i < bones.Length; i++)
+        {
+            boneTransforms[i].Position = bones[i].localPosition;
+            boneTransforms[i].Rotation = bones[i].localRotation;
+        }
+    }
+
+    private void PopulateAnimationStartBoneTransforms(string clipName, BoneTransform[] boneTransforms)
+    {
+        Vector3 positionBeforeSampling = transform.position;
+        Quaternion rotationBeforeSampling = transform.rotation;
+
+        foreach (AnimationClip clip in animator.runtimeAnimatorController.animationClips)
+        {
+            if (clip.name == clipName)
+            {
+                clip.SampleAnimation(gameObject, 0);
+                PopulateBoneTransforms(boneTransforms);
+                break;
+            }
+        }
+
+        transform.position = positionBeforeSampling;
+        transform.rotation = rotationBeforeSampling;
+    }
+
+    private string GetStandUpStateName()
+    {
+        return isFacingUp ? faceUpstandUpStateName : faceDownstandUpStateName;
+    }
+
+    private BoneTransform[] GetStandUpBonesTransforms()
+    {
+        return isFacingUp ? faceUpStandUpBoneTransforms : faceDownStandUpBoneTransforms;
     }
 }
